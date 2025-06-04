970632807d577482f1e57e36983765ce1c6049fc
thanh binh <donguuyenthanhbinh160704@gmail.com>
2025-06-04 22:24:14 +0700

    Dev Binh - Hoàn thành

---
+++ Operation.java
@@ -0,0 +1,17 @@
+public class Operation {
+    private int a;
+    private int b;
+
+    public Operation(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
